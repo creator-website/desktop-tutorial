5db774abf84479dfb7928344f60c918b8be57583
creator-website <mari.sitkovskaya@mail.ru>
2023-02-22 21:10:09 +0300

    Fixing the js code in a theme change

--- 2_month/script.js
+++ 2_month/script.js
@@ -1,11 +1,5 @@
 function toggleMode() {
-    var body = document.getElementsByTagName("body")[0];
-    if (body.classList.contains("dark-theme")) {
-      body.classList.remove("dark-theme");
-      body.classList.add("llight-theme");
-    } else {
-      body.classList.remove("light-theme");
-      body.classList.add("dark-theme");
-    }
-  }
-  
+  var body = document.getElementsByTagName("body")[0];
+  body.classList.toggle("dark-theme");
+  body.classList.toggle("light-theme");
+}
