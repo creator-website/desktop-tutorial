748352ca82906f27ec42f0ab669180c78f671be9
creator-website <mari.sitkovskaya@mail.ru>
2023-03-03 00:56:44 +0300

    the theme depends on the selected color scheme in the system. And theme changes are saved

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-function toggleMode() {
-  var body = document.getElementsByTagName("body")[0];
-  body.classList.toggle("dark-theme");
-  body.classList.toggle("light-theme");
+const body = document.querySelector('body');
+const themeSelect = document.querySelector('#theme-select');
+
+// При загрузке страницы проверяем, есть ли сохраненная тема в localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  body.classList.add(savedTheme);
+  themeSelect.value = savedTheme;
+} else {
+  body.classList.add('auto-theme');
 }
+
+// Обрабатываем изменение выбранного значения в списке
+themeSelect.addEventListener('change', () => {
+  const selectedTheme = themeSelect.value;
+
+  if (selectedTheme === 'auto-theme') {
+    // Проверяем, какая тема у пользователя в системе
+    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark-theme'
+      : 'light-theme';
+
+    body.classList.remove('dark-theme', 'light-theme');
+    body.classList.add(systemTheme);
+  } else {
+    body.classList.remove('auto-theme', 'dark-theme', 'light-theme');
+    body.classList.add(selectedTheme);
+  }
+
+  // Сохраняем выбранную тему в localStorage
+  localStorage.setItem('theme', selectedTheme);
+});
